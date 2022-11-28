00fba8ac690fd1938f629a269918615905578cc5
傅泉明 <67837343@qq.com>
2022-11-28 14:02:59 +0800

    update 1.0.3

---
+++ fqm-file/fqm-file-minio-spring-boot-starter/src/main/java/com/fqm/framework/file/config/MinioFileAutoConfiguration.java
@@ -0,0 +1,52 @@
+package com.fqm.framework.file.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+import com.fqm.framework.file.FileFactory;
+import com.fqm.framework.file.minio.MinioService;
+import com.fqm.framework.file.template.MinioFileTemplate;
+
+import io.minio.MinioClient;
+
+/**
+ * 
+ * name=enable 且值为true，时才加载，matchIfMissing=true 缺失也加载
+ * @version 
+ * @author 傅泉明
+ */
+@Configuration
+@ConditionalOnClass(MinioClient.class)
+@ConditionalOnProperty(prefix = "minio", name = "enabled", havingValue = "true")
+@EnableConfigurationProperties({ MinioProperties.class })
+public class MinioFileAutoConfiguration {
+
+    private final MinioProperties properties;
+
+    public MinioFileAutoConfiguration(MinioProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MinioClient getMinioClient() {
+        return MinioClient.builder()
+                .endpoint(this.properties.getEndpoint(), this.properties.getPort(), this.properties.getSecure())
+                .credentials(this.properties.getAccessKey(), this.properties.getSecretKey()).build();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @Order(200)
+    public MinioFileTemplate minioFileTemplate(FileFactory fileFactory, MinioClient minioClient, MinioProperties minioProperties) {
+        MinioFileTemplate minioFileTemplate = new MinioFileTemplate(new MinioService(minioClient), minioProperties.getBucketDefaultName());
+        fileFactory.addFileTemplate(minioFileTemplate);
+        return minioFileTemplate;
+    }
+
+}
