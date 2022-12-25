0c26e93f76738044adb61fe69a0d74d656cdb690
傅泉明 <67837343@qq.com>
2022-12-25 21:09:03 +0800

    update testEmqxMq

--- test/test-module-mq/test-module-mq-emqx/src/main/java/com/fqm/test/mq/controller/EmqxMqController.java
+++ test/test-module-mq/test-module-mq-emqx/src/main/java/com/fqm/test/mq/controller/EmqxMqController.java
@@ -6,42 +6,33 @@ import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fqm.framework.mq.MqFactory;
-import com.fqm.framework.mq.MqMode;
 import com.fqm.framework.mq.annotation.MqListener;
 import com.fqm.framework.mq.client.producer.SendCallback;
 import com.fqm.framework.mq.client.producer.SendResult;
+import com.fqm.framework.mq.util.MqProducer;
 import com.fqm.test.controller.BaseController;
 import com.fqm.test.model.User;
-import com.fqm.test.mq.config.EmqxMqProperties;
 
 @RestController
-@EnableConfigurationProperties(EmqxMqProperties.class)
 public class EmqxMqController extends BaseController {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Resource
     MqFactory mqFactory;
-    @Value("${mq.mqs.g.binder:}")
-    private String mqBinder;
-    @Value("${mq.mqs.g1.binder:}")
-    private String mqBinder1;
     
-    @Value("${mq.mqs.g.topic:}")
-    private String topic;
-    @Value("${mq.mqs.g1.topic:}")
-    private String topic1;
-
+    public static final String BUSINESS_CREATE_ORDER = "g";
+    public static final String BUSINESS_CREATE_ORDER_1 = "g1";
+    public static final String BUSINESS_CREATE_ORDER_DEAD = "g-dead";
+    public static final String BUSINESS_CREATE_ORDER_DEAD_1 = "g1-dead";
     @Resource
-    EmqxMqProperties emqxMqProperties;
-
-    @MqListener(name = "${mq.mqs.g.name}")
+    MqProducer mqProducer;
+    
+    @MqListener(name = BUSINESS_CREATE_ORDER)
     public void receiveMessage1(String message) {
         logger.info("receiveMessage---emqx---1=" + message);
 //        if (true) {
@@ -49,7 +40,7 @@ public class EmqxMqController extends BaseController {
 //        }
     }
 
-    @MqListener(name = "${mq.mqs.g1.name}")
+    @MqListener(name = BUSINESS_CREATE_ORDER)
     public void receiveMessage2(String message) {
         logger.info("receiveMessage---emqx---2=" + message);
 //        if (true) {
@@ -57,24 +48,32 @@ public class EmqxMqController extends BaseController {
 //        }
     }
     
-    @MqListener(name = "${mq.mqs.g-dead.name}")
+    @MqListener(name = BUSINESS_CREATE_ORDER_DEAD)
     public void mqDLQ(String message) {
         logger.info("emqx.DLQ=" + message);
     }
     
-    @MqListener(name = "${mq.mqs.g1-dead.name}")
+    @MqListener(name = BUSINESS_CREATE_ORDER_DEAD_1)
     public void mqDLQ1(String message) {
         logger.info("emqx1.DLQ=" + message);
+    }
+    
+    private String getBinder() {
+        return mqProducer.getBinder(BUSINESS_CREATE_ORDER);
+    }
+    
+    private String getTopic() {
+        return mqProducer.getTopic(BUSINESS_CREATE_ORDER);
     }
 
     @GetMapping("/mq/emqx/sendMessage")
     public Object sendEmqxMessage() {
         User user = getUser();
         try {
-            boolean flag = mqFactory.getMqTemplate(emqxMqProperties.getBinder()).syncSend(emqxMqProperties.getTopic(), user);
+            boolean flag = mqProducer.syncSend(BUSINESS_CREATE_ORDER, user);
             logger.info("emqx.send->{}", flag);
             
-            mqFactory.getMqTemplate(mqBinder1).asyncSend(topic1, user, new SendCallback() {
+            mqFactory.getMqTemplate(getBinder()).asyncSend(getTopic(), user, new SendCallback() {
                 @Override
                 public void onSuccess(SendResult sendResult) {
                     System.out.println("onSuccess");
@@ -85,7 +84,6 @@ public class EmqxMqController extends BaseController {
                     System.out.println("onException");
                 }
             });
-//            logger.info("emqx.send->{}", flag1);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -96,12 +94,12 @@ public class EmqxMqController extends BaseController {
     public Object sendEmqxDelayMessage() {
         User user = getUser();
         try {
-            boolean flag = mqFactory.getMqTemplate(MqMode.EMQX).syncDelaySend(topic, user, 3, TimeUnit.SECONDS);
+            boolean flag = mqProducer.syncDelaySend(BUSINESS_CREATE_ORDER, user, 3, TimeUnit.SECONDS);
             logger.info("emqx.sendDelay->{}", flag);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return user;
     }
-
+    
 }
