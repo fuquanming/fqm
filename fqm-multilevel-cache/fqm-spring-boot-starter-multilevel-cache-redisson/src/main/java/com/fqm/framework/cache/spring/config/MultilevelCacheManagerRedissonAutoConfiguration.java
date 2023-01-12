da4acba7e8eff9bbec91439e84b6e0fe1d6d35a3
傅泉明 <67837343@qq.com>
2023-01-12 14:05:31 +0800

    update 1.0.3

--- fqm-multilevel-cache/fqm-spring-boot-starter-multilevel-cache-redisson/src/main/java/com/fqm/framework/cache/spring/config/MultilevelCacheManagerRedissonAutoConfiguration.java
+++ fqm-multilevel-cache/fqm-spring-boot-starter-multilevel-cache-redisson/src/main/java/com/fqm/framework/cache/spring/config/MultilevelCacheManagerRedissonAutoConfiguration.java
@@ -2,13 +2,12 @@ package com.fqm.framework.cache.spring.config;
 
 import org.redisson.api.RedissonClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ApplicationObjectSupport;
 
-import com.fqm.framework.common.redisson.RedissonConfig;
 import com.fqm.framework.common.redisson.RedissonFactory;
 
 /**
@@ -20,16 +19,14 @@ import com.fqm.framework.common.redisson.RedissonFactory;
 @Configuration
 public class MultilevelCacheManagerRedissonAutoConfiguration extends ApplicationObjectSupport {
     
-    @Bean
-    @ConditionalOnMissingBean
-    @ConfigurationProperties(prefix = "spring.redis")
-    public RedissonConfig redissonProperties() {
-        return new RedissonConfig();
-    }
-    
-    @Bean
-    @ConditionalOnMissingBean
-    public RedissonClient redissonClient(RedissonConfig redissonProperties) {
-        return RedissonFactory.getClient(redissonProperties);
+    /**
+     * 使用 redisson-spring-boot-starter 初始化RedissonClient，不用初始化RedissonConfig及RedissonClient，
+     * 会自动初始化RedissonClient，使用spring.redis配置或spring.redis.redisson配置
+     * @return
+     */
+    @Bean(destroyMethod = "shutdown")
+    @ConditionalOnMissingBean(RedissonClient.class)
+    RedissonClient redissonClient(RedisProperties redisProperties) {
+        return RedissonFactory.getClient(redisProperties);
     }
 }
