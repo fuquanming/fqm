0d111691879d08caf7a9a2f9d55fa7e6f2658e9b
傅泉明 <67837343@qq.com>
2023-01-12 13:50:27 +0800

    update 1.0.5

--- fqm-common/fqm-common-redisson/src/main/java/com/fqm/framework/common/redisson/RedissonFactory.java
+++ fqm-common/fqm-common-redisson/src/main/java/com/fqm/framework/common/redisson/RedissonFactory.java
@@ -1,5 +1,6 @@
 package com.fqm.framework.common.redisson;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,13 +8,19 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 /**
  * 
- * 生成RedissonClient
+ * 生成 RedissonClient
+ * 如果要 RedisTemplate、StringRedisTemplate，使用Redisson连接
+ * 在父工程中使用 redisson-spring-boot-starter。注意版本号
  * @version 
  * @author 傅泉明
  */
 public class RedissonFactory {
+    
+    private static final String REDIS_PROTOCOL_PREFIX = "redis://";
+    private static final String REDISS_PROTOCOL_PREFIX = "rediss://";
     
     private RedissonFactory() {
     }
@@ -22,42 +29,62 @@ public class RedissonFactory {
      * 返回单机或集群的客户端 
      * @return
      */
-    public static RedissonClient getClient(RedissonConfig redissonProperties) {
-        Config config = null;
-        if (redissonProperties.getCluster() == null) {
-            config = getSingleConfig(redissonProperties);
+    public static RedissonClient getClient(RedisProperties redisProperties) {
+        Duration timeoutValue = redisProperties.getTimeout();
+        int timeout;
+        if(null == timeoutValue){
+            timeout = 10000;
         } else {
-            config = getClusterConfig(redissonProperties);
+            timeout = (int)timeoutValue.toMillis();
+        }
+        
+        Config config = new Config().setCodec(JsonJacksonCodec.INSTANCE);
+        // 哨兵
+        if (null != redisProperties.getSentinel()) {
+            String[] nodes = convert(redisProperties.getSentinel().getNodes());
+            config = new Config();
+            // 字符编码
+            config.useSentinelServers()
+                .setMasterName(redisProperties.getSentinel().getMaster())
+                .addSentinelAddress(nodes)
+                .setDatabase(redisProperties.getDatabase())
+                .setConnectTimeout(timeout)
+                .setUsername(redisProperties.getUsername())
+                .setPassword(redisProperties.getPassword());
+        } else if (null != redisProperties.getCluster()) {
+            // 集群
+            String[] nodes = convert(redisProperties.getCluster().getNodes());
+            config.useClusterServers()
+                .addNodeAddress(nodes)
+                .setConnectTimeout(timeout)
+                .setUsername(redisProperties.getUsername())
+                .setPassword(redisProperties.getPassword());
+        } else {
+            // 单机
+            String prefix = REDIS_PROTOCOL_PREFIX;
+            if (redisProperties.isSsl()) {
+                prefix = REDISS_PROTOCOL_PREFIX;
+            }
+            config.useSingleServer()
+                .setAddress(prefix + redisProperties.getHost() + ":" + redisProperties.getPort())
+                .setDatabase(redisProperties.getDatabase())
+                .setConnectTimeout(timeout)
+                .setUsername(redisProperties.getUsername())
+                .setPassword(redisProperties.getPassword());
         }
         return Redisson.create(config);
     }
     
-    public static Config getSingleConfig(RedissonConfig redissonProperties) {
-        Config config = new Config();
-        // 字符编码
-        config.setCodec(JsonJacksonCodec.INSTANCE) 
-        .useSingleServer().setAddress("redis://" + redissonProperties.getHost() + ":" + redissonProperties.getPort())
-        .setDatabase(redissonProperties.getDatabase())
-        .setUsername(redissonProperties.getUsername())
-        .setPassword(redissonProperties.getPassword())
-        .setSslEnableEndpointIdentification(redissonProperties.isSsl());
-        return config;
-    }
-    
-    public static Config getClusterConfig(RedissonConfig redissonProperties) {
-        List<String> clusterNodes = new ArrayList<>();
-        for (int i = 0; i < redissonProperties.getCluster().getNodes().size(); i++) {
-            clusterNodes.add("redis://" + redissonProperties.getCluster().getNodes().get(i));
+    private static String[] convert(List<String> nodesObject) {
+        List<String> nodes = new ArrayList<>(nodesObject.size());
+        for (String node : nodesObject) {
+            if (!node.startsWith(REDIS_PROTOCOL_PREFIX) && !node.startsWith(REDISS_PROTOCOL_PREFIX)) {
+                nodes.add(REDIS_PROTOCOL_PREFIX + node);
+            } else {
+                nodes.add(node);
+            }
         }
-        Config config = new Config();
-        // 字符编码
-        config.setCodec(JsonJacksonCodec.INSTANCE)
-        .useClusterServers()
-        .addNodeAddress(clusterNodes.toArray(new String[clusterNodes.size()]))
-        .setUsername(redissonProperties.getUsername())
-        .setPassword(redissonProperties.getPassword())
-        .setSslEnableEndpointIdentification(redissonProperties.isSsl());
-        return config;
+        return nodes.toArray(new String[nodes.size()]);
     }
     
 }
